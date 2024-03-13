669ba63823d405119d93475cb61f49fe0154d02f
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-03-13 15:37:24 +0100

    before backend changing

---
+++ client_react/src/api.js
@@ -0,0 +1,13 @@
+//URL's and enpoints
+
+//URL and ENPOINTS for local developing
+// export const URL = 'http://localhost:5111'
+// export const iTEM_ENDPOINT = "api/itemapi";
+// export const USER_ENDPOINT = 'api/userapi';
+
+
+//URL and ENPOINTS for publish
+export const URL = 'https://AIASwebapi.azurewebsites.net'
+export const iTEM_ENDPOINT = "api/itemapi";
+export const USER_ENDPOINT = 'api/userapi';
+
